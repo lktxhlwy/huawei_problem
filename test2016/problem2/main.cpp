522609c5b5bf2cca254c391adc77ddfb6f0767c2
lk940918 <744729139@qq.com>
2017-07-05 16:40:52 +0800

    完成了问题1和2

--- test2016/problem2/main.cpp
+++ test2016/problem2/main.cpp
@@ -1,17 +1,20 @@
 #include<iostream>
 #include <stdlib.h>
 #include <string.h>
+#include <vector>
 using namespace std;
+using std::vector;
+
 typedef struct {
 	char *filename;
-	char filepath[100];
+	char filepath[1000];
 	int line;
 	int count;
 }errorlog;
 
 char * get_filename(char* filepath)
 {
-	
+
 	int pathlength = strlen(filepath);
 	int namelength = pathlength;
 	for (int i = pathlength - 1; i >= 0; i--) {
@@ -43,16 +46,16 @@ void print_filename(char* filename)
 
 
 int main(void) {
-	errorlog log[8];
-	int totalcount = 0;
+	vector<errorlog> log;
 	errorlog temp;
 	int sameflag = 0;
+
 	while (cin >> temp.filepath >> temp.line) {
 		sameflag = 0;
 		temp.filename = get_filename(temp.filepath);
 		int i = 0;
-		for (i = 0; i < totalcount; i++) {
-			if ((strcmp(log[i].filename,temp.filename)==0) && (log[i].line == temp.line)) {
+		for (i = 0; i < log.size(); i++) {
+			if ((strcmp(log[i].filename, temp.filename) == 0) && (log[i].line == temp.line)) {
 				log[i].count++;
 				sameflag = 1;
 				break;
@@ -61,21 +64,31 @@ int main(void) {
 		if (sameflag == 1) {
 			continue;
 		}
-		else if ((totalcount <= 8) && (i == totalcount))
-		{
-			log[i] = temp;
+		else{
+			log.push_back(temp);
 			log[i].count = 1;
-			totalcount++;
 		}
 	}
 
-	for (int j = 0; j < totalcount; j++)
+	int printsize = (log.size() > 8) ? 8 : log.size();
+	for (int i = 0; i < printsize; i++)
 	{
-		print_filename(log[j].filename);
-		cout << " " << log[j].line << " " << log[j].count;
+		int maxNum = 0;
+		int max = 0;
+		int j = 0;
+		for (j = 0; j < log.size(); j++) {
+			if (log[j].count > max) {
+				max = log[j].count;
+				maxNum = j;
+			}
+		}
+		print_filename(log[maxNum].filename);
+		cout << " " << log[maxNum].line << " " << log[maxNum].count;
 		cout << endl;
+		vector<errorlog>::iterator k = log.begin() + maxNum;
+		log.erase(k);
+
 	}
 
 	return 0;
-
 }
--- test2016/test2016/main.cpp
+++ test2016/test2016/main.cpp
@@ -3,37 +3,40 @@ using namespace std;
 int main(void)
 {
 	int n, m;
-	cin >> n >> m;
-	int grade[30001];
-	int temp;
-	for (int i = 1; i <= n; i++) {
-		cin >> temp;
-		grade[i] = temp;
-	}
-	char cmd;
-	int a, b;
-	int max;
-	for (int i = 1; i <= m; i++) {
-		max = 0;
-		cin >> cmd;
-		cin >> a >> b;
-		if (cmd == 'Q') {
-			if (a <= b) {
-				for (int j = a; j <= b; j++) {
-					if (grade[j] > max)
-						max = grade[j];
-				}
-			}
-			else {
-				for (int j = b; j <= a; j++) {
-					if (grade[j] > max)
-						max = grade[j];
-				}
-			}
-			cout << max << endl;
+	while (cin >> n >> m) {
+		int grade[30001];
+		int temp;
+		for (int i = 1; i <= n; i++) {
+			cin >> temp;
+			grade[i] = temp;
 		}
-		else if (cmd == 'U') {
-			grade[a] = b;
+		char cmd;
+		int a, b;
+		int max;
+		for (int i = 1; i <= m; i++) {
+			max = 0;
+			cin >> cmd;
+			cin >> a >> b;
+			if (cmd == 'Q') {
+				if (a <= b) {
+					for (int j = a; j <= b; j++) {
+						if (grade[j] > max)
+							max = grade[j];
+					}
+				}
+				else {
+					for (int j = b; j <= a; j++) {
+						if (grade[j] > max)
+							max = grade[j];
+					}
+				}
+				cout << max << endl;
+			}
+			else if (cmd == 'U') {
+				grade[a] = b;
+			}
 		}
 	}
+
+	return 0;
 }
